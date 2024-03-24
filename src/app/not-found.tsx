08f26c50d4293cdc1277ab85271a06ee3c698150
Hyeongjin Kim <kimjin5537@daum.net>
2024-03-24 13:26:08 +0900

    GJ-16 not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,29 @@
+"use client";
+
 import { NextPage } from "next";
 import Link from "next/link";
 
+import Lottie from "react-lottie-player";
+
+import notFound from "../../public/not-found.json";
+
+import style from "./not-found.module.css";
+
 const NotFount: NextPage = () => {
   return (
-    <div>
-      <div>이 페이지는 존재하지 않습니다.</div>
-      <Link href="/">홈으로</Link>
-    </div>
+    <main className={style.main}>
+      <div className={style.title}>이 페이지는 존재하지 않습니다</div>
+      <Lottie
+        loop
+        animationData={notFound}
+        play
+        style={{ width: 300, height: 300 }}
+      />
+
+      <Link href="/">
+        <div className={style.btn}>홈으로</div>
+      </Link>
+    </main>
   );
 };
 
